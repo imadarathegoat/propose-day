5884db5626c23c91dc20c3c4706629e8c663beb0
ibhaveshsakpal <iambhaveshsakpal@gmail.com>
2026-02-07 15:59:11 +0530

    updated code

--- script.js
+++ script.js
@@ -16,20 +16,13 @@ function createSparkles() {
   for (let i = 0; i < 30; i++) {
     const sparkle = document.createElement("div");
     sparkle.className = "sparkle";
-
     const x = (Math.random() - 0.5) * 200 + "px";
     const y = (Math.random() - 0.5) * 200 + "px";
-
     sparkle.style.setProperty("--x", x);
     sparkle.style.setProperty("--y", y);
-
     sparkle.style.top = "50%";
     sparkle.style.left = "50%";
-
     sparklesContainer.appendChild(sparkle);
-
-    setTimeout(() => {
-      sparkle.remove();
-    }, 1000);
+    setTimeout(() => { sparkle.remove(); }, 1000);
   }
 }
